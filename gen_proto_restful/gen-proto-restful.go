be5e132c33e1cdd24f58a6a31bbb5897330e7af1
lzx <1453536003@qq.com>
2023-03-11 16:52:14 +0800

    init

---
+++ gen_proto_restful/gen-proto-restful.go
@@ -0,0 +1,44 @@
+package gen_proto_restful
+//
+//import (
+//	"flag"
+//	"fmt"
+//	gen2 "github.com/huaixiaohai/tools/cmd/protoc-gen-restful/gen"
+//	"google.golang.org/protobuf/compiler/protogen"
+//	"google.golang.org/protobuf/types/pluginpb"
+//)
+//
+////
+//const (
+//	version = "1.2.0"
+//
+//	//contextPackage = protogen.GoImportPath("context")
+//)
+//var requireUnimplemented *bool
+//
+//func GetProtoRestful() {
+//
+//		showVersion := flag.Bool("version", false, "print the version and exit")
+//		flag.Parse()
+//		if *showVersion {
+//			fmt.Printf("protoc-gen-go-grpc %v\n", version)
+//			return
+//		}
+//
+//		var flags flag.FlagSet
+//		requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
+//
+//		protogen.Options{
+//			ParamFunc: flags.Set,
+//		}.Run(func(gen *protogen.Plugin) error {
+//			gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+//			for _, f := range gen.Files {
+//				if !f.Generate {
+//					continue
+//				}
+//				gen2.GenerateFile(gen, f)
+//			}
+//			return nil
+//		})
+//
+//}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/huaixiaohai/gen
+
+go 1.17
+
+require (
+	github.com/golang/protobuf v1.5.3
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4
+	google.golang.org/protobuf v1.29.0
+)
